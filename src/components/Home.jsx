ff83d07355060c0726bb92e951a621e772c1ac29
Brandon Roberts <bbrenterprise25@gmail.com>
2023-07-09 15:34:01 -0500

    lil change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { animateElements, checkSlide } from '../animations.js';
 import {Link} from 'react-scroll'
+import logo from '../assets/logo3.svg'
 
 const Home = () => {
 
@@ -15,7 +16,7 @@ const Home = () => {
 
   return (
     <div id='home' className='w-full h-screen flex justify-center items-center flex-col mx-auto'>
-      <img className='top-0 left-0 w-full h-screen object-cover' src='../dist/assets/logo3.svg' alt="logo" />
+      <img className='top-0 left-0 w-full h-screen object-cover' src={logo} alt="logo" />
       <div className='flex flex-nowrap flex-col justify-center items-center w-full text-center'>
         <h3 className='text-lg md:text-3xl text-stone-400 font-bold -mt-24 uppercase slide-right'>Web Design <span className='text-stone-700'>&nbsp;|&nbsp;</span> App Creation <span className='text-stone-700'>&nbsp;|&nbsp;</span> Innovation
         </h3>
