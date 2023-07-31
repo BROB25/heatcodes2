36681c3042ee46a4e4cd2884f3d90ce3e1f0e76c
BROB25 <bbrenterprise25@gmail.com>
2023-07-30 20:48:40 -0500

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
     <div id='home' className='flex justify-center items-center flex-col mx-auto'>
       <img className='top-0 left-0 w-[200px] md:w-[500px] md:h-full my-12' src={logo} alt="logo" />
       <div className='flex flex-nowrap flex-col justify-center items-center w-full text-center'>
-        <h4 className='text-base md:text-xl text-stone-400 font-bold uppercase'>Web Design Services <span className='text-stone-700'>&nbsp;|&nbsp;</span> App Creation <span className='text-stone-700'>&nbsp;|&nbsp;</span> Innovation
+        <h4 className='text-base md:text-xl text-stone-400 font-bold uppercase'>Web Design Service$ <span className='text-stone-700'>&nbsp;|&nbsp;</span> App Creation <span className='text-stone-700'>&nbsp;|&nbsp;</span> Innovation
         </h4>
         <Link to='services' smooth={true} duration={500}>
           <button className='mt-8 slide-left hover:scale-110 transition-all duration-300 shadow-lg'>View Services</button>
